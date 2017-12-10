3f402504fc2021a5557a7b69cef0072732bce00e
Radziu-SH <grochowicz.radoslaw@protonmail.com>
2017-12-10 21:44:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziedziczenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs php = new Kurs();
+            php.informacje();
+            Console.ReadKey();
+        }
+    }
+    class Produkty
+    {
+        public void informacje(string nazwa, double cena, string kategoria)
+        {
+            Console.WriteLine(nazwa);
+            Console.WriteLine(cena);
+            Console.WriteLine(kategoria);
+        }
+
+
+    }
+    class Kurs : Produkty
+    {
+        int rozdzialy = 80;
+        public void informacje()
+        {
+            base.informacje("test", 42.2, "internet");
+            Console.WriteLine(rozdzialy);
+        }
+    }
+
+
+       
+}
